d44c8de80522bf511e4edf18662c1aac4673af47
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-05 11:21:12 +0100

    createBrowserRouter => create a router object(we passe to it our routes), to use it all our app should be inside the RouterProvider(we pass to it the router)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>,
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
