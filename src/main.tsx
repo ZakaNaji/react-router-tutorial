282cc1352490d4f815f5da3770d4539f153dc4a8
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-05 11:23:30 +0100

    add the root component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home Page</div>,
+    element: <Root />,
   },
 ]);
 createRoot(document.getElementById("root")!).render(
